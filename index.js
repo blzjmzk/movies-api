98730461076244a5868070421606b44b963544ec
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-01-03 16:34:22 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json()); //dodajemy middleware by parsowało json w req body
+
+const courses = [
+  { id: 1, name: "course 1" },
+  { id: 2, name: "course 2" },
+  { id: 3, name: "course 3" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+}); //odpowiadamy na request wysyłając wiadomość
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(course);
+  res.send(course);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("The course with a given ID was not found");
+  res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port v${port}`));
