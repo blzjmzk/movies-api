bee6a9cab3f06e7df95434ab9f344feaeacec436
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-01-08 14:20:52 +0100

    Cleaning unnecessary middleware

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-const startupDebugger = require("debug")("app:startup");
-const dbDebugger = require("debug")("app:db");
-const config = require("config");
-const helmet = require("helmet");
-const morgan = require("morgan");
 const Joi = require("joi");
 const express = require("express");
 const app = express();
-const logger = require("./middleware/logger");
-const authenticator = require("./middleware/authenticator");
 const genres = require("./routes/genres");
 const home = require("./routes/home");
 
-app.set("view engine", "pug");
-app.set("views", "./views");
-
 app.use(express.json()); //dodajemy middleware by parsowało json w req body
-app.use(helmet());
-app.use(logger);
-app.use(authenticator);
+
 app.use("/api/genres", genres);
 app.use("/", home);
-
-//Configuration
-console.log("Application Name: " + config.get("name"));
-console.log("Mail Server: " + config.get("mail.host"));
-
-if (app.get("env") === "development") {
-  app.use(morgan("tiny"));
-  startupDebugger("Morgan enabled...");
-}
 
 function validateGenre(genre) {
   const schema = Joi.object({
