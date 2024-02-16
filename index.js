5f0545653b07381a9db4a7ee431571d75059aea6
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-02-16 13:08:36 +0100

    Connect API to MongoDB

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const Joi = require("joi");
 const express = require("express");
 const app = express();
 const genres = require("./routes/genres");
 const home = require("./routes/home");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/movies-api")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.log("Could not connect to MongoDB..."));
 
 app.use(express.json()); //dodajemy middleware by parsowało json w req body
 
 app.use("/api/genres", genres);
 app.use("/", home);
-
-function validateGenre(genre) {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
-  return schema.validate(genre);
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port v${port}`));
--- routes/genres.js
+++ routes/genres.js
@@ -1,65 +1,76 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Documentary" },
-  { id: 3, name: "Sci-fi" },
-];
+const Genre = mongoose.model(
+  "Genre",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+  })
+);
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const genres = await Genre.find().sort("name");
   res.send(genres);
 });
 
-router.post("/", (req, res) => {
+router.get("/:id", async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+
+  if (!genre)
+    return res.status(404).send("The genre with a given ID was not found");
+
+  res.send(genre);
+});
+
+router.post("/", async (req, res) => {
   const { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = {
-    id: genres.length + 1,
-    name: req.body.name,
-  };
+  let genre = new Genre({ name: req.body.name });
 
-  genres.push(genre);
+  genre = await genre.save();
   res.send(genre);
 });
 
-router.get("/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with a given ID was not found");
-  res.send(genre);
-});
-
-router.put("/:id", (req, res) => {
-  //spradzamy czy gatunek istnieje, jezeli nie – zwracamy błąd 404
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
-  if (!genre)
-    return res.status(404).send("The genre with a given ID was not found");
-
-  // Walidacja żądania, jeśli invalid zwracamy błąd
+router.put("/:id", async (req, res) => {
   const { error } = validateGenre(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  //aktualizujemy element
-  genre.name = req.body.name;
-
-  //zwracamy element klientowi
-  res.send(genre);
-});
-
-router.delete("/:id", (req, res) => {
-  //sprawdzamy, czy gatunek istnieje, jęśli nie zwracamy błąd
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  const genre = await Genre.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    {
+      new: true,
+    }
+  );
   if (!genre)
     return res.status(404).send("The genre with a given ID was not found");
 
-  //usuwamy kurs
-  const index = genres.indexOf(genre); //znajdujemy index kursu do usunięcia
-  genres.splice(index, 1); //usuwamy kurs o danym indeksie
-
-  //zwracamy usunięty kurs
   res.send(genre);
 });
+
+router.delete("/:id", async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id);
+
+  if (!genre)
+    return res.status(404).send("The genre with a given ID was not found");
+
+  res.send(genre);
+});
+
+function validateGenre(genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(genre);
+}
 
 module.exports = router;
