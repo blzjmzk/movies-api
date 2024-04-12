83727833de05eb60b89ceef39e19c97f3c3d6669
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-04-12 14:14:43 +0200

    Add movies api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const genres = require("./routes/genres");
+const movies = require("./routes/movies");
 const customers = require("./routes/customers");
 const home = require("./routes/home");
 const mongoose = require("mongoose");
@@ -14,6 +15,7 @@ app.use(express.json()); //dodajemy middleware by parsowało json w req body
 
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
 app.use("/", home);
 
 const port = process.env.PORT || 3000;
